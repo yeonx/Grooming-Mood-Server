c7fe1f34b1f6df0e9da8b3f7a5ff3a59d5998472
yeonhee <ydol2@naver.com>
2022-09-30 19:17:58 +0900

    Feat : 다이어리 entity 구현

---
+++ src/main/java/com/be/grooming_mood/diary/domain/Diary.java
@@ -0,0 +1,27 @@
+package com.be.grooming_mood.diary.domain;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "diary")
+public class Diary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "diary_title", nullable = false)
+    private String diaryTitle;
+
+    @Column(name = "diary_content", nullable = false)
+    private String diaryContent;
+
+
+}
