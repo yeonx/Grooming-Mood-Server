0e70f8c2b36de08f2627b7040787bf6672ec926b
yeonhee <ydol2@naver.com>
2022-09-30 16:36:25 +0900

    Initial Commit

---
+++ src/main/java/com/be/grooming_mood/GroomingMoodApplication.java
@@ -0,0 +1,13 @@
+package com.be.grooming_mood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroomingMoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroomingMoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/be/grooming_mood/GroomingMoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.grooming_mood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroomingMoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
