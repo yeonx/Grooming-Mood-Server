4040e878df58ed4c9b95b4520eb6b9c23747096d
yeonhee <ydol2@naver.com>
2022-10-02 05:05:00 +0900

    Feat : QueryDSL 설정 및 다이어리 repo 구현

---
+++ build/generated/sources/annotationProcessor/java/main/com/be/grooming_mood/diary/domain/QDiary.java
@@ -0,0 +1,41 @@
+package com.be.grooming_mood.diary.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QDiary is a Querydsl query type for Diary
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QDiary extends EntityPathBase<Diary> {
+
+    private static final long serialVersionUID = 1463752858L;
+
+    public static final QDiary diary = new QDiary("diary");
+
+    public final StringPath diaryContent = createString("diaryContent");
+
+    public final StringPath diaryTitle = createString("diaryTitle");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public QDiary(String variable) {
+        super(Diary.class, forVariable(variable));
+    }
+
+    public QDiary(Path<? extends Diary> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QDiary(PathMetadata metadata) {
+        super(Diary.class, metadata);
+    }
+
+}
+
