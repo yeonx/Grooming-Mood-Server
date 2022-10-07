b4d49ba2836769f031252bd92a02ed4f76d320e3
yeonhee <ydol2@naver.com>
2022-10-08 05:04:18 +0900

    Feat : 리스트 조회시 단순정보 쿼리 (유저 X)

--- src/main/java/com/be/grooming_mood/diary/infra/DiaryQueryDao.java
+++ src/main/java/com/be/grooming_mood/diary/infra/DiaryQueryDao.java
@@ -1,14 +1,12 @@
 package com.be.grooming_mood.diary.infra;
 
-import com.be.grooming_mood.diary.application.criteria.DiaryDetailInfoCriteria;
-import com.be.grooming_mood.diary.application.criteria.DiarySimpleInfoCriteria;
-import com.be.grooming_mood.diary.application.criteria.QDiaryDetailInfoCriteria;
-import com.be.grooming_mood.diary.application.criteria.QDiarySimpleInfoCriteria;
+import com.be.grooming_mood.diary.application.criteria.*;
 import com.be.grooming_mood.diary.domain.QDiary;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.be.grooming_mood.diary.domain.QDiary.diary;
@@ -45,4 +43,29 @@ public class DiaryQueryDao {
                         ).get(diaryId)
         );
     }
+    public DiaryListQueryResult findAllDiaryList(String cursor, int size){
+        List<DiarySimpleInfoCriteria> infoList = queryFactory
+                .select(new QDiarySimpleInfoCriteria(diary.id, diary.diaryContent))
+                .from(diary)
+                .limit(size + 1)
+                .orderBy(diary.id.desc())
+                .fetch();
+        boolean hasNext = hasNext(size, infoList);
+        String nextCursor = hasNext ? getDiaryIdNextCursor(infoList) : null;
+        return new DiaryListQueryResult(infoList,hasNext,nextCursor);
+    }
+    private boolean hasNext(int size, List<DiarySimpleInfoCriteria> infoList){
+        boolean hasNext = false;
+        if(infoList.size() >size){
+            infoList.remove(size);
+            hasNext = true;
+        }
+        return hasNext;
+    }
+
+    private String getDiaryIdNextCursor(List<DiarySimpleInfoCriteria> infoList){
+        long lastDiaryId = infoList.get(infoList.size() - 1).getDiaryId();
+        return String.format("%020d",lastDiaryId);
+    }
+
 }
