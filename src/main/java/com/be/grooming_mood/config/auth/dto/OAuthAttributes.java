e551b7fa0c45e9b608d80a8efc7395afed0a21dd
kimdoha <kkhhjj3393@gmail.com>
2022-10-10 20:59:53 +0900

    Feat: Naver Security 설정 등록

--- src/main/java/com/be/grooming_mood/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/be/grooming_mood/config/auth/dto/OAuthAttributes.java
@@ -36,6 +36,9 @@ public class OAuthAttributes {
             String registrationId,
             String userNameAttributeName,
             Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -47,6 +50,21 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .profileImg((String) attributes.get("profileImg"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(
+            String userNameAttributeName,
+            Map<String, Object> attributes){
+
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .profileImg((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
