8c205d1a97ea8ad5adc3474785d78987a4124e2f
kimdoha <kkhhjj3393@gmail.com>
2022-10-10 17:32:48 +0900

    Feat: OAuth 로그인 Test 화면

---
+++ src/main/java/com/be/grooming_mood/web/IndexController.java
@@ -0,0 +1,27 @@
+package com.be.grooming_mood.web;
+
+import com.be.grooming_mood.user.domain.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final HttpSession httpSession;
+
+    @GetMapping("/")
+    public String index(Model model){
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
+        return "index";
+    }
+}
