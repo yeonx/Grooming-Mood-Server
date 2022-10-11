71e7a583cc621c6b6a5cf3109d1f80ff5f3773c6
yeonhee <ydol2@naver.com>
2022-10-11 16:54:48 +0900

    Feat : 일기 수정 컨트롤러 구현

--- src/main/java/com/be/grooming_mood/diary/presentation/DiaryController.java
+++ src/main/java/com/be/grooming_mood/diary/presentation/DiaryController.java
@@ -1,10 +1,15 @@
 package com.be.grooming_mood.diary.presentation;
 
 import com.be.grooming_mood.diary.application.DiaryCommandService;
+import com.be.grooming_mood.diary.application.command.DiaryUpdateCommand;
 import com.be.grooming_mood.diary.presentation.dto.DiaryCreateDto;
 import com.be.grooming_mood.diary.presentation.dto.DiaryDtoMapper;
+import com.be.grooming_mood.user.domain.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,8 +19,24 @@ public class DiaryController {
     private final DiaryDtoMapper diaryDtoMapper;
 
     @PostMapping("/{userId}")
-    public long createDiary(@PathVariable("userId") Long userId, @RequestBody DiaryCreateDto diaryCreateDto){
+    public long createDiary(@PathVariable("userId") Long userId,@Valid DiaryCreateDto diaryCreateDto){
         return diaryCommandService.create(userId, diaryDtoMapper.toCreateCommand(diaryCreateDto));
+    }
+
+    @PostMapping("/{userId}/{diaryId}")
+    public void updateDiary(@PathVariable("userId") Long userId, @PathVariable("diaryId") Long diaryId
+            , Authentication authentication, @Valid DiaryUpdateCommand diaryUpdateCommand){
+//        try{
+//            User user = (User) authentication.getPrincipal();
+//            if (user.getId() == userId) {
+//                diaryCommandService.update(diaryId,diaryUpdateCommand);
+//            }else{
+//
+//            }
+//        } catch (Exception e) {
+//
+//        }
+        diaryCommandService.update(diaryId,diaryUpdateCommand);
     }
 
 
