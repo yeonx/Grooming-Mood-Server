f6e13b2bcc8dc7e421eaf2a46f2a1ecac060dda3
yeonhee <ydol2@naver.com>
2022-10-13 01:54:54 +0900

    Feat : 에러 관련 구현

--- src/main/java/com/be/grooming_mood/diary/application/DiaryQueryService.java
+++ src/main/java/com/be/grooming_mood/diary/application/DiaryQueryService.java
@@ -1,11 +1,22 @@
 package com.be.grooming_mood.diary.application;
 
+import com.be.grooming_mood.diary.application.criteria.DiarySimpleInfoCriteria;
+import com.be.grooming_mood.diary.infra.DiaryQueryDao;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class DiaryQueryService {
+
+    private final DiaryQueryDao diaryQueryDao;
+
+    @Transactional(readOnly = true)
+    public DiarySimpleInfoCriteria findSimpleInfo(long diaryId){
+        return diaryQueryDao.findSimpleInfo(diaryId)
+                .orElseThrow();
+    }
 
 
 }
