a00618dd3314825691962ac31121eae58148086d
kimdoha <kkhhjj3393@gmail.com>
2022-10-14 02:59:43 +0900

    Feat: FeelingHistory domain 생성

---
+++ src/main/java/com/be/grooming_mood/feeling/domain/FeelingHistory.java
@@ -0,0 +1,36 @@
+package com.be.grooming_mood.feeling.domain;
+
+import com.be.grooming_mood.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name="feelingHistory")
+public class FeelingHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+
+    @Column(name = "feeling", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private FeelingType feeling;
+
+    @Builder
+    public FeelingHistory(User user,
+                          FeelingType feeling){
+        this.user = user;
+        this.feeling = feeling;
+    }
+}
