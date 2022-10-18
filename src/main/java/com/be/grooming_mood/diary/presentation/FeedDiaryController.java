ca8da51b17ec83efb63cf9fe85db1639a2f9f142
yeonhee <ydol2@naver.com>
2022-10-19 02:45:23 +0900

    Feat : /feed-diary 권한 추가

---
+++ src/main/java/com/be/grooming_mood/diary/presentation/FeedDiaryController.java
@@ -0,0 +1,14 @@
+package com.be.grooming_mood.diary.presentation;
+
+import com.be.grooming_mood.diary.application.DiaryCommandService;
+import com.be.grooming_mood.diary.application.DiaryQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/feed-diary")
+public class FeedDiaryController {
+
+}
