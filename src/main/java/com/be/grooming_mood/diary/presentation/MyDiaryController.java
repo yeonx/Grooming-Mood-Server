447599e92493e85ff148054cf8807b2e1614bb6a
yeonhee <ydol2@naver.com>
2022-10-19 03:26:31 +0900

    Chore : 일기 수정 controller

--- src/main/java/com/be/grooming_mood/diary/presentation/MyDiaryController.java
+++ src/main/java/com/be/grooming_mood/diary/presentation/MyDiaryController.java
@@ -2,6 +2,7 @@ package com.be.grooming_mood.diary.presentation;
 
 import com.be.grooming_mood.diary.application.DiaryCommandService;
 import com.be.grooming_mood.diary.application.DiaryQueryService;
+import com.be.grooming_mood.diary.application.command.DiaryUpdateCommand;
 import com.be.grooming_mood.diary.application.criteria.DiaryDetailInfoCriteria;
 import com.be.grooming_mood.diary.application.criteria.DiaryListQueryResult;
 import com.be.grooming_mood.diary.presentation.dto.DiaryCreateDto;
@@ -38,8 +39,9 @@ public class MyDiaryController {
         return diaryQueryService.findDetailInfo(diaryId);
     }
 
-    /* 수정 */
-//    @PostMapping("/{diaryId}")
+//    /* 수정 */ -> 방식에 따라 고려
+//    @ApiOperation(value = "일기 수정")
+//    @PostMapping("/{userId}/{diaryId}")
 //    public void updateDiary(@PathVariable("diaryId") Long diaryId,@Valid DiaryUpdateCommand diaryUpdateCommand){
 //        diaryCommandService.update(diaryId,diaryUpdateCommand);
 //    }
