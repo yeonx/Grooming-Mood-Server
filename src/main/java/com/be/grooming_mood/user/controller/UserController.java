9899dac1349e292764fcd6ad0169c1f71d6d3d55
kimdoha <kkhhjj3393@gmail.com>
2022-10-18 20:17:47 +0900

    Feat: 유저 회원 가입 변경

--- src/main/java/com/be/grooming_mood/user/controller/UserController.java
+++ src/main/java/com/be/grooming_mood/user/controller/UserController.java
@@ -2,11 +2,15 @@ package com.be.grooming_mood.user.controller;
 
 
 import com.be.grooming_mood.user.domain.User;
+import com.be.grooming_mood.user.dto.PostLoginRes;
+import com.be.grooming_mood.user.dto.UserCreateDto;
+import com.be.grooming_mood.user.dto.UserLoginDto;
 import com.be.grooming_mood.user.dto.UserUpdateDto;
 import com.be.grooming_mood.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
@@ -14,12 +18,21 @@ import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/user")
 public class UserController {
-    private final HttpSession httpSession;
     private final UserService userService;
 
+    @PostMapping("/signup")
+    public Long signUp(@Valid UserCreateDto userCreateDto) {
+        return userService.signUpUser(userCreateDto);
+    }
 
 
+//    @PostMapping("/login")
+//    public PostLoginRes login(@Valid UserLoginDto userLoginDto) {
+//        PostLoginRes loginUser = userService.login(userLoginDto);
+//        return loginUser;
+//    }
 
 //    @GetMapping("/user-info")
 //    public User getUserInfo() {
