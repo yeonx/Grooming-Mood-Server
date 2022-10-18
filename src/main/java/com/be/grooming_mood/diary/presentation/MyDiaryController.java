b203ec56ff82459977eadc7d8cfa5c51318c1caa
yeonhee <ydol2@naver.com>
2022-10-19 02:53:15 +0900

    Feat : 일기 상세보기 로직 추가

--- src/main/java/com/be/grooming_mood/diary/presentation/MyDiaryController.java
+++ src/main/java/com/be/grooming_mood/diary/presentation/MyDiaryController.java
@@ -2,6 +2,7 @@ package com.be.grooming_mood.diary.presentation;
 
 import com.be.grooming_mood.diary.application.DiaryCommandService;
 import com.be.grooming_mood.diary.application.DiaryQueryService;
+import com.be.grooming_mood.diary.application.criteria.DiaryDetailInfoCriteria;
 import com.be.grooming_mood.diary.application.criteria.DiaryListQueryResult;
 import com.be.grooming_mood.diary.presentation.dto.DiaryCreateDto;
 import com.be.grooming_mood.user.service.UserService;
@@ -29,6 +30,12 @@ public class MyDiaryController {
     @GetMapping("/{userId}")
     public DiaryListQueryResult getMyDiaryList(@PathVariable("userId") Long userId){
         return diaryQueryService.findMyDiaryList(userId);
+    }
+
+    @ApiOperation(value ="내 상세 보기")
+    @GetMapping("/{diaryId}")
+    public DiaryDetailInfoCriteria getDiaryInfo(@PathVariable long diaryId){
+        return diaryQueryService.findDetailInfo(diaryId);
     }
 
     /* 수정 */
