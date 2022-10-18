3439ab6b0dab26991489aea1360e0fc5d60c8e2b
kimdoha <kkhhjj3393@gmail.com>
2022-10-18 20:23:14 +0900

    Feat: 유저 회원가입 기능 (이메일 중복 검증)

--- src/main/java/com/be/grooming_mood/exception/ErrorCode.java
+++ src/main/java/com/be/grooming_mood/exception/ErrorCode.java
@@ -4,13 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
     DIARY_NOT_FOUND(NOT_FOUND, "해당 다이어리를 찾을 수 없습니다."),
-    USER_NOT_FOUND(NOT_FOUND, "해당 유저를 찾을 수 없습니다.");
+    USER_NOT_FOUND(NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
+
+    INVALID_EMAIL(BAD_REQUEST, "중복된 이메일입니다.");
 
     private final HttpStatus httpStatus;
     private final String detail;
--- src/main/java/com/be/grooming_mood/user/service/UserService.java
+++ src/main/java/com/be/grooming_mood/user/service/UserService.java
@@ -1,20 +1,56 @@
 package com.be.grooming_mood.user.service;
 
+import com.be.grooming_mood.exception.NotFoundException;
+import com.be.grooming_mood.user.domain.Role;
 import com.be.grooming_mood.user.domain.User;
 import com.be.grooming_mood.user.domain.UserRepository;
-import com.be.grooming_mood.user.dto.UserInfoResponseDto;
+import com.be.grooming_mood.user.dto.PostLoginRes;
+import com.be.grooming_mood.user.dto.UserCreateDto;
+import com.be.grooming_mood.user.dto.UserLoginDto;
 import com.be.grooming_mood.user.dto.UserUpdateDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
+
+import static com.be.grooming_mood.exception.ErrorCode.INVALID_EMAIL;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public Long signUpUser(UserCreateDto userCreateDto) {
+
+        Optional<User> emailCheck = userRepository.findByEmail(userCreateDto.getEmail());
+
+        if(emailCheck.isPresent()){
+            throw new NotFoundException(INVALID_EMAIL);
+        }
+
+        User user = User.builder()
+                        .email(userCreateDto.getEmail())
+                        .password(userCreateDto.getPassword())
+                        .name(userCreateDto.getName())
+                        .role(Role.USER)
+                        .build();
+        userRepository.save(user);
+        return user.getId();
+    }
+
+    // 로그인 구현
+//    @Transactional
+//    public PostLoginRes login(UserLoginDto userLoginDto) {
+//
+//    }
 
     @Transactional
     public void updateUser(Long userId, UserUpdateDto userUpdateDto) {
@@ -36,6 +72,7 @@ public class UserService {
 
         return user;
     }
+
 
 
 }
