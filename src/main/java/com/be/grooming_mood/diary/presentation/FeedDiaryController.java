f0e823048d5fa2f7cee583e1c3dd5e3f2da70596
yeonhee <ydol2@naver.com>
2022-10-19 02:47:52 +0900

    Feat : 페이징 처리 controller

--- src/main/java/com/be/grooming_mood/diary/presentation/FeedDiaryController.java
+++ src/main/java/com/be/grooming_mood/diary/presentation/FeedDiaryController.java
@@ -10,5 +10,47 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/feed-diary")
 public class FeedDiaryController {
+    private final DiaryQueryService diaryQueryService;
+    private final DiaryCommandService diaryCommandService;
+
+/**
+ * 페이징
+ */
+//    @GetMapping("/my-page")
+//    public DiaryListQueryResult getMyDiaryList(@RequestParam(required = false) String cursor,
+//                                               @RequestParam(required = false, defaultValue = "10") int size){
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        return diaryQueryService.findMyDiaryList(user.getId(),cursor,size);
+//    }
+//
+//    @GetMapping("/all-paging")
+//    public DiaryListQueryResult getAllDiaryList(@RequestParam(required = false) String cursor,
+//                                               @RequestParam(required = false, defaultValue = "10") int size){
+//        return diaryQueryService.findAllDiaryList(cursor,size);
+//    }
+//
+//    @GetMapping("/happy-paging")
+//    public DiaryListQueryResult getHappyDiaryList(@RequestParam(required = false) String cursor,
+//                                                @RequestParam(required = false, defaultValue = "10") int size){
+//        return diaryQueryService.findHappyDiaryList(cursor,size);
+//    }
+//
+//    @GetMapping("/sad-paging")
+//    public DiaryListQueryResult getSadDiaryList(@RequestParam(required = false) String cursor,
+//                                                  @RequestParam(required = false, defaultValue = "10") int size){
+//        return diaryQueryService.findSadDiaryList(cursor,size);
+//    }
+//
+//    @GetMapping("/normal-paging")
+//    public DiaryListQueryResult getNormalDiaryList(@RequestParam(required = false) String cursor,
+//                                                @RequestParam(required = false, defaultValue = "10") int size){
+//        return diaryQueryService.findNormalDiaryList(cursor,size);
+//    }
+//
+//    @GetMapping("/angry-paging")
+//    public DiaryListQueryResult getAngryDiaryList(@RequestParam(required = false) String cursor,
+//                                                   @RequestParam(required = false, defaultValue = "10") int size){
+//        return diaryQueryService.findAngryDiaryList(cursor,size);
+//    }
 
 }
