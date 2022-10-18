15168385547c59ba44b249a870c26d36dbaf6bdc
kimdoha <kkhhjj3393@gmail.com>
2022-10-18 23:46:35 +0900

    Chore: Feeling History 코드 정리

--- src/main/java/com/be/grooming_mood/feeling/controller/FeelingHistoryController.java
+++ src/main/java/com/be/grooming_mood/feeling/controller/FeelingHistoryController.java
@@ -1,12 +1,11 @@
 package com.be.grooming_mood.feeling.controller;
 
 import com.be.grooming_mood.feeling.dto.FeelingHistoryDto;
+import com.be.grooming_mood.feeling.service.FeelingHistoryService;
 import com.be.grooming_mood.oauth.LoginUser;
 import com.be.grooming_mood.feeling.dto.FeelingHistoryCreateDto;
-import com.be.grooming_mood.feeling.service.FeelingHistoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.util.List;
 
@@ -14,12 +13,14 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/feeling-history")
 public class FeelingHistoryController {
+
     private final FeelingHistoryService feelingHistoryService;
 
-//    @GetMapping("/feeling-history")
-//    public List<FeelingHistoryDto> getFeelingHistoryInWeek() {
-//        return feelingHistoryService.getFeelingHistory(user.getId());
-//    }
+//
+////    @GetMapping("/feeling-history")
+////    public List<FeelingHistoryDto> getFeelingHistoryInWeek() {
+////        return feelingHistoryService.getFeelingHistory(user.getId());
+////    }
 
     @PostMapping("")
     public void createFeelingHistory(@Valid @RequestBody FeelingHistoryCreateDto feelingHistoryCreateDto) {
