16dbeafd1f1a35442e6003bf6965bb5d25a6a32e
kimdoha <kkhhjj3393@gmail.com>
2022-10-18 21:02:39 +0900

    Feat: Bad Request Error 추가
    -

--- src/main/java/com/be/grooming_mood/exception/ErrorCode.java
+++ src/main/java/com/be/grooming_mood/exception/ErrorCode.java
@@ -13,7 +13,8 @@ public enum ErrorCode {
     DIARY_NOT_FOUND(NOT_FOUND, "해당 다이어리를 찾을 수 없습니다."),
     USER_NOT_FOUND(NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
 
-    INVALID_EMAIL(BAD_REQUEST, "중복된 이메일입니다.");
+    INVALID_EMAIL(BAD_REQUEST, "중복된 이메일입니다."),
+    INVALID_PASSWORD(BAD_REQUEST, "잘못된 비밀번호입니다.");
 
     private final HttpStatus httpStatus;
     private final String detail;
