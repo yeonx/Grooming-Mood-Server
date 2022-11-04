f202d01817fadea586a164ef3ae7a124395aff54
yeonhee <ydol2@naver.com>
2022-11-04 14:09:12 +0900

    Feat : like entity 새로 생성

---
+++ src/main/java/com/be/grooming_mood/like/domain/like.java
@@ -0,0 +1,29 @@
+package com.be.grooming_mood.like.domain;
+
+import com.be.grooming_mood.diary.domain.Diary;
+import com.be.grooming_mood.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "like")
+public class like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "diary_id", nullable = false)
+    private Diary diary;
+}
