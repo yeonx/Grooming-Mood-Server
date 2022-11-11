2d7dd6327d04769699cd1b886645c02853cd998d
kimdoha <kkhhjj3393@gmail.com>
2022-11-11 15:51:29 +0900

    feat: 감정 히스토리 api operation 추가

--- src/main/java/com/be/grooming_mood/feeling/controller/FeelingHistoryController.java
+++ src/main/java/com/be/grooming_mood/feeling/controller/FeelingHistoryController.java
@@ -5,6 +5,7 @@ import com.be.grooming_mood.feeling.dto.FeelingHistoryInfo;
 import com.be.grooming_mood.feeling.dto.FeelingHistoryInfoList;
 import com.be.grooming_mood.feeling.service.FeelingHistoryService;
 import com.be.grooming_mood.feeling.dto.FeelingHistoryCreateDto;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
@@ -18,12 +19,13 @@ public class FeelingHistoryController {
 
     private final FeelingHistoryService feelingHistoryService;
 
-
+    @ApiOperation(value ="월별 감정 히스토리 조회(사용 X)")
     @GetMapping("/userId/{userId}")
     public FeelingHistoryInfoList getFeelingHistoryInMonth(@PathVariable("userId") Long userId) {
         return feelingHistoryService.getFeelingHistoryList(userId);
     }
 
+    @ApiOperation(value ="감정 히스토리 생성")
     @PostMapping("")
     public String createFeelingHistory(@Valid @RequestBody FeelingHistoryCreateDto feelingHistoryCreateDto) {
         return feelingHistoryService.createFeelingHistory(feelingHistoryCreateDto);
