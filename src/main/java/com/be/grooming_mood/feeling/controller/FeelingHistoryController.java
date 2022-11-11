912f21c5b812f76c4e705bcde9227a899134d17b
kimdoha <kkhhjj3393@gmail.com>
2022-11-11 17:08:30 +0900

    feat: 지난주와 이번주 감정 통계 api

--- src/main/java/com/be/grooming_mood/feeling/controller/FeelingHistoryController.java
+++ src/main/java/com/be/grooming_mood/feeling/controller/FeelingHistoryController.java
@@ -3,6 +3,7 @@ package com.be.grooming_mood.feeling.controller;
 import com.be.grooming_mood.feeling.domain.FeelingHistory;
 import com.be.grooming_mood.feeling.dto.FeelingHistoryInfo;
 import com.be.grooming_mood.feeling.dto.FeelingHistoryInfoList;
+import com.be.grooming_mood.feeling.dto.FeelingStatisticInfoList;
 import com.be.grooming_mood.feeling.service.FeelingHistoryService;
 import com.be.grooming_mood.feeling.dto.FeelingHistoryCreateDto;
 import io.swagger.annotations.ApiOperation;
@@ -23,6 +24,18 @@ public class FeelingHistoryController {
     @GetMapping("/userId/{userId}")
     public FeelingHistoryInfoList getFeelingHistoryInMonth(@PathVariable("userId") Long userId) {
         return feelingHistoryService.getFeelingHistoryList(userId);
+    }
+
+    @ApiOperation(value ="지난주 감정 통계 조회")
+    @GetMapping("/last-week/userId/{userId}")
+    public FeelingStatisticInfoList getFeelingInLastWeek(@PathVariable("userId") Long userId) {
+        return feelingHistoryService.getFeelingInLastWeek(userId);
+    }
+
+    @ApiOperation(value ="이번주 감정 통계 조회")
+    @GetMapping("/today-week/userId/{userId}")
+    public FeelingStatisticInfoList getFeelingInThisWeek(@PathVariable("userId") Long userId) {
+        return feelingHistoryService.getFeelingInThisWeek(userId);
     }
 
     @ApiOperation(value ="감정 히스토리 생성")
--- src/main/java/com/be/grooming_mood/feeling/repository/FeelingHistoryQueryDao.java
+++ src/main/java/com/be/grooming_mood/feeling/repository/FeelingHistoryQueryDao.java
@@ -2,9 +2,13 @@ package com.be.grooming_mood.feeling.repository;
 
 import com.be.grooming_mood.feeling.dto.FeelingHistoryInfo;
 import com.be.grooming_mood.feeling.dto.FeelingHistoryInfoList;
+import com.be.grooming_mood.feeling.dto.FeelingStatisticInfo;
+import com.be.grooming_mood.feeling.dto.FeelingStatisticInfoList;
 import com.be.grooming_mood.feeling.dto.QFeelingHistoryInfo;
+import com.be.grooming_mood.feeling.dto.QFeelingStatisticInfo;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -27,4 +31,32 @@ public class FeelingHistoryQueryDao {
 
         return new FeelingHistoryInfoList(infoList);
     }
+
+    public FeelingStatisticInfoList findAllFeelingStatisticsInLastWeek(Long userId, LocalDateTime start, LocalDateTime end) {
+
+        List<FeelingStatisticInfo> infoList =
+            jpaQueryFactory.select(new QFeelingStatisticInfo( feelingHistory.feeling, feelingHistory.feeling.count()))
+                .from(feelingHistory)
+                .where(feelingHistory.user.id.eq(userId))
+                .where(feelingHistory.createdDate.between(start, end))
+                .groupBy(feelingHistory.feeling)
+                .fetch();
+
+        return new FeelingStatisticInfoList(infoList);
+    }
+
+    public FeelingStatisticInfoList findAllFeelingStatisticsInThisWeek(Long userId, LocalDateTime start, LocalDateTime end) {
+
+        List<FeelingStatisticInfo> infoList =
+            jpaQueryFactory.select(new QFeelingStatisticInfo( feelingHistory.feeling, feelingHistory.feeling.count()))
+                .from(feelingHistory)
+                .where(feelingHistory.user.id.eq(userId))
+                .where(feelingHistory.createdDate.between(start, end))
+                .groupBy(feelingHistory.feeling)
+                .fetch();
+
+
+        return new FeelingStatisticInfoList(infoList);
+    }
+
 }
