abc37383b393f7b182785c43acbcfb2c4d1edf5c
yeonhee <ydol2@naver.com>
2022-11-11 16:24:05 +0900

    chore : 일기 좋아요 디폴트 값 설정

--- src/main/java/com/be/grooming_mood/diary/domain/Diary.java
+++ src/main/java/com/be/grooming_mood/diary/domain/Diary.java
@@ -7,6 +7,7 @@ import com.be.grooming_mood.user.domain.User;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -59,6 +60,7 @@ public class Diary extends BaseTimeEntity {
     private List<Likes> likesList;
 
     @Transient
+    @ColumnDefault("0")
     private Integer likesCount;
 
     public void updateLikesCount(Integer likesCount) {
