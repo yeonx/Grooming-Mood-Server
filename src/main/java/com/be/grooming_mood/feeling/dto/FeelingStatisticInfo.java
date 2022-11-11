b443bc4ac8fb55778e81348945ce794d8d32d40d
kimdoha <kkhhjj3393@gmail.com>
2022-11-11 17:08:15 +0900

    feat: 감정 통계 dto 생성

---
+++ src/main/java/com/be/grooming_mood/feeling/dto/FeelingStatisticInfo.java
@@ -0,0 +1,19 @@
+package com.be.grooming_mood.feeling.dto;
+
+import com.be.grooming_mood.feeling.domain.FeelingType;
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Builder;
+
+public class FeelingStatisticInfo {
+
+  private FeelingType feeling;
+  private Long amount;
+
+  @QueryProjection @Builder
+  public FeelingStatisticInfo(
+      FeelingType feeling,
+      Long amount){
+      this.feeling = feeling;
+      this.amount = amount;
+  }
+}
